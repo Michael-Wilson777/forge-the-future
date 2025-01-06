6cc9406d71dd549520a1854b536267ba61cefa5c
Michael Wilson <michaelwilson777@gmail.com>
2025-01-06 10:03:54 -0500

    first commit

---
+++ src/pages/HomePage.js
@@ -0,0 +1,17 @@
+import { Card, CardBody, CardHeader, CardText } from "react-bootstrap";
+
+
+const HomePage = () => {
+    return (
+        <>
+        <Card>
+            
+        </Card>
+            <h1>Forge the Future: </h1>
+            <h3>Your Hub for Welding/Metal Fabrication Education & Equipment</h3>
+            <p>Whether you're a newcomer to welding and metal fabrication or simply in need of the right tools to get the job done, you've found the perfect place!</p>
+        </>
+    );
+}
+
+export default HomePage;
