89dfdd9a7c9271a0a772cd8d6ac7f585877e3081
Michael Wilson <michaelwilson777@gmail.com>
2025-01-06 11:35:51 -0500

    background image and Introduction on HomePage added

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,15 +1,23 @@
-import { Card, CardBody, CardHeader, CardText } from "react-bootstrap";
+import { Container, Card, CardBody, CardHeader, CardTitle, CardText } from "react-bootstrap";
 
 
 const HomePage = () => {
     return (
+        
         <>
-        <Card>
-            
+        <Container className="home-page">
+        <Card style={{width: '30rem'}}>
+            <CardHeader style={{border: 'none'}} className="text-center">
+                <CardTitle><h1>Forge the Future</h1></CardTitle>
+                <CardText><h4>Your Hub for Welding/Metal Fabrication Education & Equipment</h4></CardText>
+            </CardHeader>
+            <CardBody className="text-center">
+                <CardText>
+                Whether you're a newcomer to welding and metal fabrication or simply in need of the right tools to get the job done, you've found the perfect place!
+                </CardText>
+            </CardBody>
         </Card>
-            <h1>Forge the Future: </h1>
-            <h3>Your Hub for Welding/Metal Fabrication Education & Equipment</h3>
-            <p>Whether you're a newcomer to welding and metal fabrication or simply in need of the right tools to get the job done, you've found the perfect place!</p>
+        </Container>
         </>
     );
 }
