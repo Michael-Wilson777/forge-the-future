07fa45b53363592a35b19b6819d0fa07c2cf04cc
Michael Wilson <michaelwilson777@gmail.com>
2025-01-17 10:59:01 -0500

    starting shop while i think about what else I want and how I want it on the Education page

--- src/pages/EducationPage.js
+++ src/pages/EducationPage.js
@@ -1,10 +1,9 @@
-import { Container } from "react-bootstrap";
 import Videos from "../components/Videos";
 const EducationPage = () => {
     return (
-        <Container>
+        
             <Videos />
-        </Container>
+        
     )
 }
 export default EducationPage;
